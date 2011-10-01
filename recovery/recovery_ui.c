848d798cc7f04f004b3ca228895f0a2aa6d5098f
codeworkx <daniel.hillenbrand@codeworkx.de>
2011-10-01 19:51:08 +0200

    default recovery keymapping

--- recovery/recovery_ui.c
+++ recovery/recovery_ui.c
@@ -46,7 +46,7 @@ int device_toggle_display(volatile char* key_pressed, int key_code) {
         return 0;
         //return get_allow_toggle_display() && (key_code == KEY_HOME || key_code == KEY_MENU || key_code == KEY_END);
     }
-    return get_allow_toggle_display() && (key_code == KEY_MENU || key_code == KEY_BACK);
+    return get_allow_toggle_display() && (key_code == KEY_HOME || key_code == KEY_MENU || key_code == KEY_POWER || key_code == KEY_END);
 }
 
 int device_reboot_now(volatile char* key_pressed, int key_code) {
@@ -59,16 +59,14 @@ int device_handle_key(int key_code, int visible) {
             case KEY_CAPSLOCK:
             case KEY_DOWN:
             case KEY_VOLUMEDOWN:
+            case KEY_MENU:
                 return HIGHLIGHT_DOWN;
 
-            case KEY_MENU:
             case KEY_LEFTSHIFT:
             case KEY_UP:
             case KEY_VOLUMEUP:
-                return HIGHLIGHT_UP;
-
             case KEY_HOME:
-                return SELECT_ITEM;
+                return HIGHLIGHT_UP;
 
             case KEY_POWER:
                 if (ui_get_showing_back_button()) {
@@ -83,12 +81,19 @@ int device_handle_key(int key_code, int visible) {
             case KEY_CENTER:
             case KEY_CAMERA:
             case KEY_F21:
-            case KEY_SEND:           
+            case KEY_SEND:
+                return SELECT_ITEM;
+            
             case KEY_END:
             case KEY_BACKSPACE:
             case KEY_SEARCH:
+                if (ui_get_showing_back_button()) {
+                    return SELECT_ITEM;
+                }
+                if (!get_allow_toggle_display())
+                    return GO_BACK;
             case KEY_BACK:
-                return NO_ACTION;
+                return GO_BACK;
         }
     }
 
